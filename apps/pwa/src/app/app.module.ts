7c7171ba02e868902031d5615ab943ded6006b66
Pascal Malbranche <11357019+perzeuss@users.noreply.github.co>
2022-01-05 02:06:05 +0100

    chore: nx migrate --run-migrations

--- apps/pwa/src/app/app.module.ts
+++ apps/pwa/src/app/app.module.ts
@@ -8,10 +8,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule.withServerTransition({ appId: 'mbat' }), IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [BrowserModule.withServerTransition({ appId: 'mbat' }), IonicModule.forRoot(), AppRoutingModule],
+    providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
