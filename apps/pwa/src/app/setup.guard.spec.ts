2ad7dad0c7ce8e38979c29313c8cdec7a00a8dc8
Pascal Malbranche <11357019+perzeuss@users.noreply.github.com>
2022-12-13 20:23:49 +0100

    test: mock router

--- apps/pwa/src/app/setup.guard.spec.ts
+++ apps/pwa/src/app/setup.guard.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SetupGuard } from './setup.guard';
+import { RouterTestingModule } from "@angular/router/testing";
 
 describe('SetupGuard', () => {
   let guard: SetupGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule]
+    });
     guard = TestBed.inject(SetupGuard);
   });
 
