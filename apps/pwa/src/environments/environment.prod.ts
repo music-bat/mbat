3aaf01d2f3fc5c4efa0f493b4cc5cf6bed7cdeef
Pascal Malbranche <11357019+perzeuss@users.noreply.github.com>
2022-12-16 18:39:13 +0100

    fix(pwa): add missing environment variables

--- apps/pwa/src/environments/environment.prod.ts
+++ apps/pwa/src/environments/environment.prod.ts
@@ -3,6 +3,11 @@ export const environment = {
   parse: {
     appId: 'mbat',
     serverUrl: 'https://api.mbat.io',
+    javascriptKey: 'YOUR_KEY_HERE',
     liveQueryUrl: 'wss://api.mbat.io'
+  },
+  spotify: {
+    clientId: 'YOUR_SPOTIFY_CLIENT_ID_HERE',
+    redirectUri: 'https://api.mbat.io/login'
   }
 };
