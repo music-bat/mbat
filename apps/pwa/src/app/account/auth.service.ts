ce9f17ecbb64b2fd37e104c1b737eaf13bda947d
Pascal Malbranche <pascal.malbranche@i22.de>
2023-02-21 22:17:01 +0100

    test: disable some tests

--- apps/pwa/src/app/account/auth.service.ts
+++ apps/pwa/src/app/account/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { AccessTokenResponse } from './types/AccessTokenResponse';
-
+import * as Parse from 'parse'
 @Injectable({
   providedIn: 'root',
 })
