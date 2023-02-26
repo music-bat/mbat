cb7db35f74eec08698601c88673eb8f942e4a9dc
Perzeuss <11357019+perzeuss@users.noreply.github.com>
2023-02-26 23:14:46 +0100

    refactor: remove unused code

--- apps/pwa/src/app/account/auth.service.ts
+++ apps/pwa/src/app/account/auth.service.ts
@@ -18,26 +18,10 @@ export class AuthService {
       // TODO: generate and verify state
       state: 'test',
     }).toString();
-    console.log(url.toString());
     window.location.replace(url.toString());
   }
 
   requestAuthorizationToken(code): Promise<AccessTokenResponse> {
     return Parse.Cloud.run('requestToken', { code });
-    // const url: URL = new URL(
-    //   'authorize',
-    //   'https://accounts.spotify.com/authorize'
-    // );
-    // url.search = new URLSearchParams({
-    //   response_type: 'token',
-    //   code,
-    //   client_id: environment.spotify.clientId,
-    //   scope: ['user-read-private', 'user-library-read'].join(' '),
-    //   redirect_uri: environment.spotify.redirectUri,
-    //   // TODO: generate and verify state
-    //   state: 'test',
-    // }).toString();
-    // console.log(url.toString());
-    // window.location.replace(url.toString());
   }
 }
