0bcf2879d8f0bd8d57b37fb589ef314901ee51c9
Perzeuss <11357019+perzeuss@users.noreply.github.com>
2023-04-02 22:42:39 +0200

    chore: cleanup code

--- apps/api/src/parse/cloud/functions/index.ts
+++ apps/api/src/parse/cloud/functions/index.ts
@@ -4,6 +4,9 @@ import { requestToken } from './request-token';
 import { refreshToken } from './refresh-token';
 import { fetchGroupPlaylist } from './query-group-playlist';
 import { importPlaylist } from './import-playlist';
+import { createGroupInvite } from './group-invite-code/create-invite-code';
+import { deleteGroupInvite } from './group-invite-code/delete-invite-code';
+import { joinGroupByInviteCode } from './group-invite-code/join-group-by-invite-code';
 
 Parse.Cloud.define('fetchGroupPlaylist', fetchGroupPlaylist);
 Parse.Cloud.define('refreshToken', refreshToken);
@@ -11,12 +14,6 @@ Parse.Cloud.define('requestToken', requestToken);
 Parse.Cloud.define('deleteFile', deleteFile);
 Parse.Cloud.define('processSyncJob', writeLibraryToDb);
 Parse.Cloud.define('importPlaylist', importPlaylist);
-
-// Define Parse Cloud functions for group invite codes
-import { createGroupInvite } from './group-invite-code/create-invite-code';
-import { deleteGroupInvite } from './group-invite-code/delete-invite-code';
-import { joinGroupByInviteCode } from './group-invite-code/join-group-by-invite-code';
-
 Parse.Cloud.define('createGroupInvite', createGroupInvite);
 Parse.Cloud.define('deleteGroupInvite', deleteGroupInvite);
 Parse.Cloud.define('joinGroupByInviteCode', joinGroupByInviteCode);
